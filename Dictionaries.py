700d05c86d0c47f6f90959eab13c4b9419e18897
ovicmp <dc8povi@gmail.com>
2021-05-12 20:52:50 +0300

    Working on last Dictionaries task
    working on "Health index > 5 and sex is 'F'", Sets finished.

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,3 +1,6 @@
+# from collections import defaultdict
+# from collections.abc import Iterable
+
 
 males_2017 = [('Belgium', 2017, 'M', 21.8), ('Czechia', 2017, 'M', 13.6), ('Denmark', 2017, 'M', 36.5), ('Germany', 2017, 'M', 15.4), ('Ireland', 2017, 'M', 45.9), ('Greece', 2017, 'M', 44.1), ('Spain', 2017, 'M', 26.7), ('France', 2017, 'M', 14.4), ('Croatia', 2017, 'M', 27.5), ('Italy', 2017, 'M', 13.2), ('Cyprus', 2017, 'M', 54.9), ('Latvia', 2017, 'M', 5.1), ('Luxembourg', 2017, 'M', 22.1), ('Hungary', 2017, 'M', 5.9), ('Malta', 2017, 'M', 68), ('Netherlands', 2017, 'M', 21.3), ('Austria', 2017, 'M', 31.9), ('Poland', 2017, 'M', 0), ('Portugal', 2017, 'M', 17.8), ('Slovenia', 2017, 'M', 14.7), ('Slovakia', 2017, 'M', 2.9), ('Finland', 2017, 'M', 27.3), ('Sweden', 2017, 'M', 24.8), ('Iceland', 2017, 'M', 32.2), ('Norway', 2017, 'M', 37.4), ('Switzerland', 2017, 'M', 29), ('United Kingdom', 2017, 'M', 39), ('Montenegro', 2017, 'M', 24.3), ('North Macedonia', 2017, 'M', 10.9), ('Serbia', 2017, 'M', 11.2)]
 
@@ -7,42 +10,94 @@ females_2017 = [('Belgium', 2017, 'F', 29), ('Czechia', 2017, 'F', 18.6), ('Denm
 
 females_2018 = [('Belgium', 2018, 'F', 26.8), ('Czechia', 2018, 'F', 18.3), ('Denmark', 2018, 'F', 24.3), ('Germany', 2018, 'F', 17.3), ('Ireland', 2018, 'F', 44.8), ('Greece', 2018, 'F', 43.8), ('Spain', 2018, 'F', 20.5), ('France', 2018, 'F', 22.3), ('Croatia', 2018, 'F', 27.6), ('Italy', 2018, 'F', 12.8), ('Cyprus', 2018, 'F', 43.4), ('Latvia', 2018, 'F', 6), ('Luxembourg', 2018, 'F', 22.2), ('Hungary', 2018, 'F', 14.2), ('Malta', 2018, 'F', 21.8), ('Netherlands', 2018, 'F', 20.7), ('Austria', 2018, 'F', 32.2), ('Poland', 2018, 'F', 14.3), ('Portugal', 2018, 'F', 8.3), ('Slovenia', 2018, 'F', 17.7), ('Slovakia', 2018, 'F', 20.4), ('Finland', 2018, 'F', 16.5), ('Sweden', 2018, 'F', 27), ('Iceland', 2018, 'F', 34.6), ('Norway', 2018, 'F', 28.1), ('Switzerland', 2018, 'F', 34.6), ('United Kingdom', 2018, 'F', 32.6), ('Montenegro', 2018, 'F', 37.4), ('North Macedonia', 2018, 'F', 27.6), ('Serbia', 2018, 'F', 20.5)]
 
-# convert tuple into dict
+# convert tuple into dict with for loop
 
-males_2017_dict = {country: [year, sex, index] for (country, year, sex, index) in males_2017}
+"""dict_males_2017_test = {}
+for country, year, sex, index in males_2017:
+    dict_males_2017_test[country] = [year, sex, index]
 
-males_2018_dict = {country: [year, sex, index] for (country, year, sex, index) in males_2018}
+print("People from males_2017: ", dict_males_2017_test)
+
+dict_males_2017_test2 = {country[0]: country[1:] for country in males_2017}
+
+print("test ", dict_males_2017_test2)"""
+
+# convert tuple into dict with dict comprehensions
+males_2017_dict = {country: [year, sex, index] for country, year, sex, index in males_2017}
+
+males_2018_dict = {country: [year, sex, index] for [country, year, sex, index] in males_2018}
 
 females_2017_dict = {country: [year, sex, index] for (country, year, sex, index) in females_2017}
 
 females_2018_dict = {country: [year, sex, index] for (country, year, sex, index) in females_2018}
 
-print('***********************************')
+print("People form males_2017 with dict comprehensions: ", males_2017_dict)
+
+# two dicts that group all data by country for each year > health_index_2017 = {‘France’: [sex, health_index]}
 dict_2017_all = males_2017_dict, females_2017_dict
 dict_2018_all = males_2018_dict, females_2018_dict
-print(dict_2017_all)
-print(dict_2018_all)
 
-print('***********************************')
+print("Users from dict 2017 male and female, health_index_2017 =", dict_2017_all)
+print("Users from dict 2018 male and female, health_index_2018 =", dict_2018_all)
+
+#  one dict that groups all data by year for Germany > germany = {2017: [sex, health_index]}
+germany_males_2017 = {
+    year: [sex, index]
+    for country, year, sex, index in males_2017 if country == 'Germany'
+}
+
+germany_females_2017 = {
+    year: [sex, index]
+    for country, year, sex, index in females_2017 if country == 'Germany'
+}
+germany_males_2018 = {
+    year: [sex, index]
+    for country, year, sex, index in males_2018 if country == 'Germany'
+}
+
+germany_females_2018 = {
+    year: [sex, index]
+    for country, year, sex, index in females_2018 if country == 'Germany'
+}
+
+germany = germany_males_2017, germany_females_2017, germany_females_2018, germany_males_2018
 
 
-males_2017_dict_germmany = {country: [year, sex, index] for (country, year, sex, index) in males_2017 if country == 'Germany'}
-
-males_2018_dict_germmany = {country: [year, sex, index] for (country, year, sex, index) in males_2018 if country == 'Germany'}
-
-females_2017_dict_germmany = {country: [year, sex, index] for (country, year, sex, index) in females_2017 if country == 'Germany'}
-
-females_2018_dict_germmany = {country: [year, sex, index] for (country, year, sex, index) in females_2018 if country == 'Germany'}
-
-final_germany = males_2017_dict_germmany, males_2018_dict_germmany, females_2017_dict_germmany, females_2018_dict_germmany
-
-final_germany2 = (dict_2017_all , dict_2018_all)
-
-print(final_germany)
-print('***********************************')
+print("Dictionary for german male and female: germany =", germany)
 
 
+# Concatenate Country and year for dictionary "health_index = {‘France_2017’: [year, sex, health_index]}"
+health_index = {
+    country + str("_2017"): [year, sex, index]
+    for country, year, sex, index in males_2017
+}
 
-# health_index = {country_year: [year, sex, index] for (country_year, year, sex, index) in males_2017 if index > 5}
-# health_index_Female = {country_year: [year, sex, index] for (country_year, year, sex, index) in males_2017 if index > 5 and sex == 'F'}
+health_index_female_2017 = {
+    country + str("_2017"): [year, sex, index]
+    for country, year, sex, index in females_2017
+}
 
+health_index_2017 = health_index, health_index_female_2017
+
+print("Concatenate country_year for male and female 2017: ", health_index, health_index_female_2017)
+
+
+# Health index > 5  starting from the previous health_index dict
+
+health_index_5 = dict()
+
+for key, value in health_index.items():
+    if int(value[2]) > 5:
+        health_index_5[key] = value
+
+print("Countries with health index > 5 : ", health_index_5)
+
+# Health index > 5 and sex is 'F'
+
+health_index_Female = dict()
+
+for key, value in health_index_Female.items():
+    if int(value[2]) > 5 and str(value[1]):
+        health_index_5[key] = value
+
+print("Health index for Country > 5 and sex is F health_index =", health_index_female_2017)
--- Sets.py
+++ Sets.py
@@ -1,8 +1,8 @@
 # set1 = {9, 2, 4, 6, 8, 22, 14, 16, 21, 20}
 # set2 = {1, 2, 3, 5, 6, 9, 22, 14, 17, 22}
 
-set1 = set([9, 2, 4, 6, 8, 22, 14, 16, 21, 20])
-set2 = set([1, 2, 3, 5, 6, 9, 22, 14, 17, 22])
+set1 = {9, 2, 4, 6, 8, 22, 14, 16, 21, 20}
+set2 = {1, 2, 3, 5, 6, 9, 22, 14, 17, 22}
 
 # union
 print("Union :", set1 | set2)
@@ -15,3 +15,59 @@ print("Difference :", set1 - set2)
 
 # symmetric difference
 print("Symmetric difference :", set1 ^ set2)
+
+# from the Database
+cities = [
+    ('Berlin', 1234, 4321.1),  # 0 # name, population, surface
+    ('Hamburg', 2345, 5432.2),  # 1
+    ('Munich', 3456, 6543.3)  # 2
+]
+
+# select name, surface from ...
+second_cities = [
+        ('Berlin', 4321.1),  # 0 # name, population, surface
+        ('Hamburg', 5432.2),  # 1
+        ('Munich', 6543.3)  # 2
+    ]
+
+# 'name': [population, surface]
+cities_by_name = {
+    # structure for: key: value
+    city: [population, surface]
+    # condition, what do I need to do ?
+    for (city, population, surface) in cities
+}
+print(cities_by_name)
+
+cities_surface = {
+    city: surface
+    for city, _, surface in cities
+}
+print(cities_surface)
+
+cities_populations = {
+        city: population
+        for city, population, _ in cities
+    }
+print(cities_populations)
+
+    # city: surface, surface > 5000
+big_cities = {
+        city: surface
+        for city, _, surface in cities
+        if surface > 5000
+    }
+print(big_cities)
+
+big_cities_as_list = [
+        surface
+        for _, _, surface in cities
+        if surface > 5000
+    ]
+print(big_cities_as_list)
+
+surface_dictionary = {
+    city: str(surface) + ' km2'
+    for city, _, surface in cities
+}
+print(surface_dictionary)
